360c7cae58b0e727c8e2a37850be54f21fbe63e5
Lorenzo <templarec@gmail.com>
2021-07-11 14:16:54 +0200

    pulizia index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
